cc91320d9222e634303872e3ed1705935c5e46db
Leonardo Machado PC <leonardo.canto@edu.pucrs.br>
2020-04-27 20:31:18 -0300

    Ex2 / Ex3  Somatorio e Fibo

--- Recursao/src/Ex2.java
+++ Recursao/src/Ex2.java
@@ -1,2 +1,15 @@
 public class Ex2 {
+    public static int somatorio(int n){
+        int som = -1; // -1 indica erro
+        if ( n == 0 || n == 1)
+            som = 1;
+        else if (n >= 2)
+            som = n = n+somatorio(n+1);
+        return som;
+    }
+
+    public static void main (String[] args){
+        System.out.println(somatorio(5));
+    }
 }
+
--- Recursao/src/Ex3.java
+++ Recursao/src/Ex3.java
@@ -1,2 +1,19 @@
 public class Ex3 {
+    public static int fibonnacci(int n)
+    {
+        if(n==1){
+            return 0;
+        }
+        if(n==2){
+            return 1;
+        }
+        else if(n>2){
+            return fibonnacci(n-1)+fibonnacci(n-2);
+        }
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(fibonnacci(10));
+    }
 }
