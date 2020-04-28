d9b5634bb7191ea003816ac7d5b419e469c195be
Leonardo Machado PC <leonardo.canto@edu.pucrs.br>
2020-04-27 21:18:35 -0300

    Ex2 Somatório

--- Recursao/src/Ex2.java
+++ Recursao/src/Ex2.java
@@ -1,11 +1,10 @@
 public class Ex2 {
     public static int somatorio(int n){
-        int som = -1; // -1 indica erro
-        if ( n == 0 || n == 1)
-            som = 1;
-        else if (n >= 2)
-            som = n = n+somatorio(n+1);
-        return som;
+        if ( n == 0 )
+            return 1;
+        else
+            return  n+somatorio(n-1);
+
     }
 
     public static void main (String[] args){
