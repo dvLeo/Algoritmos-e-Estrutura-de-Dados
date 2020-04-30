dd925c0c7006daa99ff7ccb42221ccd314b4fffc
Leonardo Machado PC <leonardo.canto@edu.pucrs.br>
2020-04-29 21:22:37 -0300

    Métodos insert e merge

--- Metodos de Ordenação/src/Metodos.java
+++ Metodos de Ordenação/src/Metodos.java
@@ -43,4 +43,48 @@ public class Metodos {
         }
         return vet;
     }
+
+    public void insertionSort(int[] vet){
+        int i, j, chave;
+        for(j = 1; j<vet.length;j++){
+            chave=vet[j];
+            i = j - 1;
+            while((i >= 0) && (vet[i] > chave)){
+                vet[i+1] = vet[i];
+                i = i - 1;
+            }
+            vet[i+1] = chave;
+        }
+    }
+
+    public void mergeSort(int[] vet){
+        mergeSort(vet, 0, vet.length-1);
+    }
+    public void mergeSort(int [] vet, int inicio, int fim){
+        if(inicio < fim){
+            int meio = (inicio + fim) / 2;
+            mergeSort(vet, inicio, meio);
+            mergeSort(vet, meio+1, fim);
+            merge(vet, inicio, meio, fim);
+        }
+    }
+
+    private void merge(int[] vet, int inicio, int meio, int fim){
+        int nL = meio - inicio+1; int nR = fim - meio;
+        int[] L = new int[nL]; int [] R = new int[nR];
+        System.arraycopy(vet, inicio, L, 0, nL);
+        System.arraycopy(vet, meio+1, R,0, nR);
+        int iL = 0; int iR = 0;
+        for(int k = inicio; k<fim; k++){
+            if(iR < nR){
+                if (iL < nL) {
+                    if (L[iL] < R[iR]) vet[k] = L[iL++];
+                    else vet[k] = R[iR++];
+                }
+                else vet[k] = R[iR++];
+            }
+            else vet[k] = L[iL++];
+        }
+        }
+    }
 }
